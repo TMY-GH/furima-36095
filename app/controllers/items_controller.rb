945a1652cf913b0d0a6bd257ebd4e9f9067b4871
TMY-GH <prog0323fujii@gmail.com>
2021-06-29 23:06:50 +0900

    ストロングパラメーターの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,9 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_days_id, :price).merge(user_id: current_user.id)
+  end
 end
