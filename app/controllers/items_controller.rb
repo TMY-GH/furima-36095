85bff816c529573621657d2f5dca58c95b725378
TMY-GH <prog0323fujii@gmail.com>
2021-06-30 06:55:37 +0900

    バリデーションの追加とエラー時入力ページに戻ってくるように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to :index
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_days_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_days_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,37 @@
 class Item < ApplicationRecord
   # Association
+  belongs_to :user
+
   has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtensionss
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :delivery_days
 
-  belongs_to :user
+  # Validation
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :delivery_days_id
+    validates :price
+  end
+  # ActiveHashはid:1は選択できない
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :delivery_days_id
+  end
+  # 価格は¥300~¥9,999,999の間
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than: 1000000}
+  # 価格は半角数字
+  validates :price, format: { with: /\A[0-9]+\z/ }
 end
