0031f7e883298a8a51ec852a7efe06f63a99c677
TMY-GH <prog0323fujii@gmail.com>
2021-06-29 23:04:08 +0900

    activeHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
   # Association
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensionss
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :delivery_days
+
   belongs_to :user
 end
