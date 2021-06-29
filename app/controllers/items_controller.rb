d9a31cfe28d960155ca38f6efb5b87fb65eb1809
TMY-GH <prog0323fujii@gmail.com>
2021-06-30 08:33:48 +0900

    出品機能にログイン制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
   def index
   end
 
@@ -16,7 +17,9 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def item_params
-    params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_days_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id,
+                                 :delivery_days_id, :price).merge(user_id: current_user.id)
   end
 end
