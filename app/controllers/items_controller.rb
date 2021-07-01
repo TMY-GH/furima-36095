d0a4be006b612c11bc4ef1ec9927c8616ffe9f28
TMY-GH <prog0323fujii@gmail.com>
2021-07-01 16:21:37 +0900

    編集後の遷移の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to show
+      redirect_to action: :show
     else
       render :new 
     end
